86544cd2da6d9ceafc126479be1e3dfce90b815a
Florian Esser <florian.esser@tngtech.com>
2024-12-01 18:46:29 +0100

    Refactor Download Dialog
    make clear, which state var belongs to which state.
    It became obvious, that this should be 3 components
    passing around the callbacks provided by the showConfirm
    event, but I will not complete this today.

--- WebUI/src/components/DownloadDialog.vue
+++ WebUI/src/components/DownloadDialog.vue
@@ -1,9 +1,8 @@
 <template>
     <div class="dialog-container z-10">
         <div class="dialog-mask absolute left-0 top-0 w-full h-full bg-black/55 flex justify-center items-center">
-            <div class="py-20 px-20 w-768px flex flex-col items-center justify-center bg-gray-600 rounded-3xl gap-8 text-white"
-                :class="{ 'animate-scale-in': animate }">
-                <div v-if="showComfirm" class="text-center flex items-center flex-col gap-5">
+            <div class="py-20 px-20 w-768px flex flex-col items-center justify-center bg-gray-600 rounded-3xl gap-8 text-white">
+                <div v-if="componentState===ComponentState.USER_CONFIRMATION" class="animate-scale-in text-center flex items-center flex-col gap-5">
                     <p>{{ i18nState.DOWNLOADER_CONFRIM_TIP }}</p>
                     <table class="text-left w-full">
                         <thead>
@@ -19,17 +18,19 @@
                             <tr v-for="item in downloadList" class="">
                                 <td>{{ item.repo_id }}</td>
                                 <td>
-                                    <span v-if="sizeRequesting" class="svg-icon i-loading w-4 h-4"></span>
+                                    <span v-if="!confirmationState.preparedAllConfirmationRequiredInformation" class="svg-icon i-loading w-4 h-4" ></span>
                                     <span v-else>{{ item.size }}</span>
                                 </td>
                                 <td>
-                                    <span v-if="sizeRequesting" class="svg-icon i-loading w-4 h-4"></span>
-                                    <svg v-if="item.gated" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1" stroke="currentColor" class="size-6 ml-2">
-                                        <path stroke-linecap="round" stroke-linejoin="round" d="M16.5 10.5V6.75a4.5 4.5 0 1 0-9 0v3.75m-.75 11.25h10.5a2.25 2.25 0 0 0 2.25-2.25v-6.75a2.25 2.25 0 0 0-2.25-2.25H6.75a2.25 2.25 0 0 0-2.25 2.25v6.75a2.25 2.25 0 0 0 2.25 2.25Z" />
-                                    </svg>
-                                    <svg v-else xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1" stroke="currentColor" class="size-6 ml-2">
-                                        <path stroke-linecap="round" stroke-linejoin="round" d="M13.5 10.5V6.75a4.5 4.5 0 1 1 9 0v3.75M3.75 21.75h10.5a2.25 2.25 0 0 0 2.25-2.25v-6.75a2.25 2.25 0 0 0-2.25-2.25H3.75a2.25 2.25 0 0 0-2.25 2.25v6.75a2.25 2.25 0 0 0 2.25 2.25Z" />
-                                    </svg>
+                                  <span v-if="!confirmationState.preparedAllConfirmationRequiredInformation" class="svg-icon i-loading w-4 h-4" ></span>
+                                  <span v-else>
+                                      <svg v-if="item.gated" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1" stroke="currentColor" class="size-6 ml-2">-->
+                                          <path stroke-linecap="round" stroke-linejoin="round" d="M16.5 10.5V6.75a4.5 4.5 0 1 0-9 0v3.75m-.75 11.25h10.5a2.25 2.25 0 0 0 2.25-2.25v-6.75a2.25 2.25 0 0 0-2.25-2.25H6.75a2.25 2.25 0 0 0-2.25 2.25v6.75a2.25 2.25 0 0 0 2.25 2.25Z" />-->
+                                      </svg>
+                                      <svg v-else xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1" stroke="currentColor" class="size-6 ml-2">-->
+                                          <path stroke-linecap="round" stroke-linejoin="round" d="M13.5 10.5V6.75a4.5 4.5 0 1 1 9 0v3.75M3.75 21.75h10.5a2.25 2.25 0 0 0 2.25-2.25v-6.75a2.25 2.25 0 0 0-2.25-2.25H3.75a2.25 2.25 0 0 0-2.25 2.25v6.75a2.25 2.25 0 0 0 2.25 2.25Z" />-->
+                                      </svg>
+                                  </span>
                                 </td>
                                 <td>
                                     <a :href="getInfoUrl(item.repo_id, item.type)" target="_blank"
@@ -61,7 +62,7 @@
                     </div>
                     <div class="flex items-center gap-2">
                         <button class="v-checkbox-control flex-none w-5 h-5"
-                            :class="{ 'v-checkbox-checked': readTerms }" @click="readTerms = !readTerms">
+                            :class="{ 'v-checkbox-checked': confirmationState.readTerms }" @click="confirmationState.readTerms = !confirmationState.readTerms">
                         </button>
                         <span class="text-sm text-left">{{ languages.DOWNLOADER_TERMS_TIP }}</span>
                     </div>
@@ -69,129 +70,165 @@
                         <button @click="cancelConfirm" class="bg-color-control-bg  py-1 px-4 rounded">{{
                             i18nState.COM_CANCEL
                         }}</button>
-                        <button @click="confirmDownload" :disabled="sizeRequesting || !readTerms || downloadList.every((i) => !i.accessGranted)"
+                        <button @click="download" :disabled="!confirmationState.preparedAllConfirmationRequiredInformation || !confirmationState.readTerms || downloadList.every((i) => !i.accessGranted)"
                             class="bg-color-active py-1 px-4 rounded">{{
                                 i18nState.COM_CONFIRM
                             }}</button>
                     </div>
                 </div>
-                <div v-else-if="hashError" class="flex flex-col items-center justify-center gap-4">
-                    <p>{{ errorText }}</p>
-                    <button @click="close" class="bg-red-500 py-1 px-4">{{ i18nState.COM_CLOSE }}</button>
-                </div>
-                <template v-else>
-                    <progress-bar :text="allDownloadTip" :percent="taskPercent" class="w-3/4"></progress-bar>
-                    <progress-bar :text="curDownloadTip" :percent="percent" class="w-3/4"></progress-bar>
-                    <button @click="cancelDownload" class="bg-red-500 py-1 px-4">{{ i18nState.COM_CANCEL }}</button>
+                <template v-if="componentState===ComponentState.DOWNLOADING">
+                    <progress-bar :text="downloadState.overallDownloadProgressHint" :percent="downloadState.overallModelDownloadsCompletedPercent" class="w-3/4"></progress-bar>
+                    <progress-bar :text="downloadState.singeDownloadProgressHint" :percent="downloadState.modelDownloadPercent" class="w-3/4"></progress-bar>
+                    <button @click="userCancelDownload" class="bg-red-500 py-1 px-4">{{ i18nState.COM_CANCEL }}</button>
                 </template>
+                <div v-if="componentState===ComponentState.ERROR" class="flex flex-col items-center justify-center gap-4">
+                  <p>{{ errorState.errorText }}</p>
+                  <button @click="closeErrorDialog" class="bg-red-500 py-1 px-4">{{ i18nState.COM_CLOSE }}</button>
+                </div>
             </div>
         </div>
     </div>
 </template>
 <script setup lang="ts">
-import { useGlobalSetup } from '@/assets/js/store/globalSetup';
+import {useGlobalSetup} from '@/assets/js/store/globalSetup';
 import ProgressBar from './ProgressBar.vue';
-import { useI18N } from '@/assets/js/store/i18n';
-import { SSEProcessor } from '@/assets/js/sseProcessor';
-import { util } from '@/assets/js/util';
-import { Const } from '@/assets/js/const';
-import { toast } from '@/assets/js/toast';
-import { useModels } from '@/assets/js/store/models';
+import {useI18N} from '@/assets/js/store/i18n';
+import {SSEProcessor} from '@/assets/js/sseProcessor';
+import {util} from '@/assets/js/util';
+import {Const} from '@/assets/js/const';
+import {toast} from '@/assets/js/toast';
+import {useModels} from '@/assets/js/store/models';
+
+
+enum ComponentState {
+  UNINITIALIZED,
+  USER_CONFIRMATION,
+  DOWNLOADING,
+  ERROR,
+  COMPLETED
+}
 
 const i18nState = useI18N().state;
 const globalSetup = useGlobalSetup();
 const models = useModels();
-let downloding = false;
-const curDownloadTip = ref("");
-const allDownloadTip = ref("");
-const percent = ref(0);
-const completeCount = ref(0);
-const taskPercent = ref(0);
-const showComfirm = ref(false);
-const sizeRequesting = ref(false);
-const hashError = ref(false);
-const errorText = ref("");
-let abortController: AbortController;
-const animate = ref(false);
+
+const downloadList = ref<DownloadModelRender[]>([]);
+const accessableDownloadList = computed(() => {return downloadList.value.filter(item => item.accessGranted && item.accessGranted === true )})
+let downloadResolve: undefined | (() => void);
+let downloadReject: ((args: DownloadFailedParams) => void) | undefined
+
+
+const componentState = ref(ComponentState.UNINITIALIZED)
+
+const confirmationState = ref(newUserConfirmationState())
+const downloadState = ref(newDownloadState())
+const errorState = ref(newErrorState())
+
+function newUserConfirmationState() {
+  return {
+    readTerms: false,
+    preparedAllConfirmationRequiredInformation: false
+  }
+}
+
+function newDownloadState() {
+  return {
+    modelDownloadPercent : 0,
+    overallModelDownloadsCompleteCount : 0,
+    overallModelDownloadsCompletedPercent : 0,
+    singeDownloadProgressHint : "",
+    overallDownloadProgressHint : "",
+    downloadAbortController : new AbortController(),
+  }
+}
+
+function newErrorState() {
+  return {
+    errorText: ""
+  }
+}
+
+
 const emits = defineEmits<{
     (e: "close"): void
 }>();
-const readTerms = ref(false);
-const downloadList = ref<DownloadModelRender[]>([]);
 
 
-function dataProcess(line: string) {
+function switchStateCleanly(newValue: ComponentState) {
+  if(componentState.value != newValue) {
+    confirmationState.value = newUserConfirmationState();
+    downloadState.value = newDownloadState();
+    errorState.value = newErrorState()
+  }
+  componentState.value = newValue
+}
+
+function onDownloadDataUpdate(line: string) {
     console.log(line);
     const dataJson = line.slice(5);
     const data = JSON.parse(dataJson) as LLMOutCallback;
     switch (data.type) {
         case "download_model_progress":
-            curDownloadTip.value = `${i18nState.COM_DOWNLOAD_MODEL} ${data.repo_id}\r\n${data.download_size}/${data.total_size} ${data.percent}% ${i18nState.COM_DOWNLOAD_SPEED}: ${data.speed}`;
-            percent.value = data.percent;
+          downloadState.value.singeDownloadProgressHint = `${i18nState.COM_DOWNLOAD_MODEL} ${data.repo_id}\r\n${data.download_size}/${data.total_size} ${data.percent}% ${i18nState.COM_DOWNLOAD_SPEED}: ${data.speed}`;
+          downloadState.value.modelDownloadPercent = data.percent;
             break;
         case "download_model_completed":
-            completeCount.value++;
-            const allTaskCount = downloadList.value.length;
-            if (completeCount.value == allTaskCount) {
-                downloding = false;
+          downloadState.value.overallModelDownloadsCompleteCount++;
+            const allTaskCount = accessableDownloadList.value.length;
+            if (downloadState.value.overallModelDownloadsCompleteCount == allTaskCount) {
+                switchStateCleanly(ComponentState.COMPLETED)
                 emits("close");
                 downloadResolve && downloadResolve();
             } else {
-                taskPercent.value = util.toFixed(completeCount.value / allTaskCount * 100, 1);
-                percent.value = 100;
-                allDownloadTip.value = `${i18nState.DOWNLOADER_DONWLOAD_TASK_PROGRESS} ${completeCount.value}/${allTaskCount}`;
+              downloadState.value.overallModelDownloadsCompletedPercent = util.toFixed(downloadState.value.overallModelDownloadsCompleteCount / allTaskCount * 100, 1);
+              downloadState.value.modelDownloadPercent = 100;
+              downloadState.value.overallDownloadProgressHint = `${i18nState.DOWNLOADER_DONWLOAD_TASK_PROGRESS} ${downloadState.value.overallModelDownloadsCompleteCount}/${allTaskCount}`;
             }
             models.refreshModels();
             break;
         case "allComplete":
-            downloding = false;
+            switchStateCleanly(ComponentState.COMPLETED)
             emits("close");
             break;
         case "error":
-            hashError.value = true;
-            abortController?.abort();
-            fetch(`${globalSetup.apiHost}/api/stopDownloadModel`)
-            downloadReject && downloadReject({ type: "error", error: errorText.value });
-
+          downloadReject && downloadReject({ type: "error", error: errorState.value.errorText });
+          downloadState.value.downloadAbortController.abort()
+          switchStateCleanly(ComponentState.ERROR)
             switch (data.err_type) {
                 case "not_enough_disk_space":
-                    errorText.value = i18nState.ERR_NOT_ENOUGH_DISK_SPACE.replace("{requires_space}", data.requires_space).replace("{free_space}", data.free_space);
+                    errorState.value.errorText = i18nState.ERR_NOT_ENOUGH_DISK_SPACE.replace("{requires_space}", data.requires_space).replace("{free_space}", data.free_space);
                     break;
                 case "download_exception":
-                    errorText.value = i18nState.ERR_DOWNLOAD_FAILED;
+                  errorState.value.errorText = i18nState.ERR_DOWNLOAD_FAILED;
                     break;
                 case "runtime_error":
-                    errorText.value = i18nState.ERROR_RUNTIME_ERROR;
+                  errorState.value.errorText = i18nState.ERROR_RUNTIME_ERROR;
                     break;
                 case "unknow_exception":
-                    errorText.value = i18nState.ERROR_GENERATE_UNKONW_EXCEPTION;
+                  errorState.value.errorText = i18nState.ERROR_GENERATE_UNKONW_EXCEPTION;
                     break;
             }
             break;
     }
 }
 
-let downloadResolve: undefined | (() => void);
-let downloadReject: ((args: DownloadFailedParams) => void) | undefined
-
 async function showConfirmDialog(downList: DownloadModelParam[], success?: () => void, fail?: (args: DownloadFailedParams) => void) {
-    if (downloding) {
-        toast.error(i18nState.DOWNLOADER_CONFLICT);
-        fail && fail({ type: "conflict" })
-        return;
+    if (componentState.value === ComponentState.DOWNLOADING) {
+      toast.error(i18nState.DOWNLOADER_CONFLICT);
+      fail && fail({ type: "conflict" })
+      return;
     }
-    animate.value = true;
-    curDownloadTip.value = i18nState.DOWNLOADER_CONFRIM_TIP;
-    showComfirm.value = true;
-    hashError.value = false;
-    percent.value = 0;
-    taskPercent.value = 0;
+    switchStateCleanly(ComponentState.USER_CONFIRMATION)
+    confirmationState.value.preparedAllConfirmationRequiredInformation = false
+
     downloadList.value = downList.map((item) => {
         return { repo_id: item.repo_id, type: item.type, size: "???", backend: item.backend , }
     });
-    readTerms.value = false;
-    downloadResolve = success;
-    downloadReject = fail;
+    success && (downloadResolve = success);
+    fail && (downloadReject = fail);
+
+    confirmationState.value.readTerms = false;
+
     try {
         const sizeResponse = await fetch(`${globalSetup.apiHost}/api/getModelSize`, {
             method: "POST",
@@ -222,17 +259,20 @@ async function showConfirmDialog(downList: DownloadModelParam[], success?: () =>
             item.gated = gatedData.gatedList[item.repo_id] || false;
             item.accessGranted = accessData.accessList[item.repo_id] || false;
         }
-        sizeRequesting.value = false;
+        confirmationState.value.preparedAllConfirmationRequiredInformation = true
     } catch (ex) {
         fail && fail({ type: "error", error: ex });
+        console.error("download model confirmation failed due with", ex)
+        switchStateCleanly(ComponentState.ERROR)
+        errorState.value.errorText = "Internal Error"
     }
 }
 
 function getInfoUrl(repoId: string, type: number) {
     if (type == 4){
-        return "https://github.com/xinntao/Real-ESRGAN" 
+        return "https://github.com/xinntao/Real-ESRGAN"
     }
-    
+
     switch(repoId){
         case "Lykon/dreamshaper-8":
             return "https://huggingface.co/spaces/CompVis/stable-diffusion-license"
@@ -248,7 +288,6 @@ function getInfoUrl(repoId: string, type: number) {
             return "https://huggingface.co/stabilityai/stable-diffusion-xl-base-1.0/blob/main/LICENSE.md"
         case "latent-consistency/lcm-lora-sdxl":
             return "https://huggingface.co/stabilityai/stable-diffusion-xl-base-1.0/blob/main/LICENSE.md"
-
     }
 
     return `https://huggingface.co/${repoId.split('/').slice(0,2).join('/')}`;
@@ -274,49 +313,41 @@ function getFunctionTip(type: number) {
 }
 
 function download() {
-    downloding = true;
-    const accessableDownloadList = downloadList.value.filter(item => item.accessGranted === true)
-    allDownloadTip.value = `${i18nState.DOWNLOADER_DONWLOAD_TASK_PROGRESS} 0/${accessableDownloadList.length}`;
-    percent.value = 0;
-    completeCount.value = 0;
-    abortController = new AbortController();
-    curDownloadTip.value = "";
+    switchStateCleanly(ComponentState.DOWNLOADING)
+    downloadState.value.overallDownloadProgressHint = `${i18nState.DOWNLOADER_DONWLOAD_TASK_PROGRESS} 0/${accessableDownloadList.value.length}`;
     fetch(`${globalSetup.apiHost}/api/downloadModel`, {
         method: "POST",
-        body: JSON.stringify(toRaw({ 'data': accessableDownloadList})),
+        body: JSON.stringify(toRaw({ 'data': accessableDownloadList.value})),
         headers: {
             "Content-Type": "application/json",
             ...(models.hfTokenIsValid ? { Authorization: `Bearer ${models.hfToken}` } : {})
         },
-        signal: abortController.signal
+        signal: downloadState.value.downloadAbortController.signal
     }).then((response) => {
         const reader = response.body!.getReader();
-        return new SSEProcessor(reader, dataProcess, undefined).start();
+        return new SSEProcessor(reader, onDownloadDataUpdate, undefined).start();
     }).catch(ex => {
         downloadReject && downloadReject({ type: "error", error: ex });
-        downloding = false;
+        switchStateCleanly(ComponentState.ERROR)
     })
 }
 
 function cancelConfirm() {
     downloadReject && downloadReject({ type: "cancelConfrim" });
+    switchStateCleanly(ComponentState.COMPLETED);
     emits("close");
 }
 
-function confirmDownload() {
-    showComfirm.value = false;
-    hashError.value = false;
-    return download();
-}
-
-function cancelDownload() {
-    abortController?.abort();
+function userCancelDownload() {
+    downloadState.value.downloadAbortController.abort();
     fetch(`${globalSetup.apiHost}/api/stopDownloadModel`)
     downloadReject && downloadReject({ type: "cancelDownload" });
+    switchStateCleanly(ComponentState.COMPLETED)
     emits("close");
 }
 
-function close() {
+function closeErrorDialog() {
+    switchStateCleanly(ComponentState.COMPLETED)
     emits("close");
 }
 

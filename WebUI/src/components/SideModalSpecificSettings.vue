63a9a8b034e9e8dac54ba7c9bd8ab90dd002e0bd
Lukas Baron <lukas.baron@tngtech.com>
2026-03-09 16:58:08 +0100

    remove demo mode blocker from sidebar

--- WebUI/src/components/SideModalSpecificSettings.vue
+++ WebUI/src/components/SideModalSpecificSettings.vue
@@ -5,36 +5,27 @@
     side="right"
     @close="$emit('close')"
   >
-    <DemoModeBlocker>
-      <SettingsChat v-show="props.mode == 'chat'" />
-    </DemoModeBlocker>
-    <DemoModeBlocker>
-      <SettingsWorkflow
-        v-show="props.mode == 'imageGen'"
-        :categories="['create-images']"
-        :title="`${mapModeToLabel(mode)} Presets`"
-      />
-    </DemoModeBlocker>
-    <DemoModeBlocker>
-      <SettingsWorkflow
-        v-show="props.mode == 'imageEdit'"
-        :categories="['edit-images']"
-        :title="`${mapModeToLabel(mode)} Presets`"
-      />
-    </DemoModeBlocker>
-    <DemoModeBlocker>
-      <SettingsWorkflow
-        v-show="props.mode == 'video'"
-        :categories="['create-videos']"
-        :title="`${mapModeToLabel(mode)} Presets`"
-      />
-    </DemoModeBlocker>
+    <SettingsChat v-show="props.mode == 'chat'" />
+    <SettingsWorkflow
+      v-show="props.mode == 'imageGen'"
+      :categories="['create-images']"
+      :title="`${mapModeToLabel(mode)} Presets`"
+    />
+    <SettingsWorkflow
+      v-show="props.mode == 'imageEdit'"
+      :categories="['edit-images']"
+      :title="`${mapModeToLabel(mode)} Presets`"
+    />
+    <SettingsWorkflow
+      v-show="props.mode == 'video'"
+      :categories="['create-videos']"
+      :title="`${mapModeToLabel(mode)} Presets`"
+    />
   </SideModalBase>
 </template>
 
 <script setup lang="ts">
 import SideModalBase from '@/components/SideModalBase.vue'
-import DemoModeBlocker from '@/components/DemoModeBlocker.vue'
 import SettingsChat from '@/components/SettingsChat.vue'
 import SettingsWorkflow from '@/components/SettingsWorkflow.vue'
 import { mapModeToLabel } from '@/lib/utils.ts'

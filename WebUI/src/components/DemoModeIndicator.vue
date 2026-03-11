2926bb3a1dc82da307a181bf1799c2d7952c8ec7
Lukas Baron <lukas.baron@tngtech.com>
2026-03-11 23:01:51 +0100

    Refactor demo banner styling

--- WebUI/src/components/DemoModeIndicator.vue
+++ WebUI/src/components/DemoModeIndicator.vue
@@ -1,11 +1,9 @@
 <template>
-  <transition name="fade">
-    <div class="demo-mode-indicator" v-if="isDemoModeOn">
-      <div class="center-popup">
-        <p>Demo mode active.</p>
-      </div>
+  <teleport to="body">
+    <div v-if="isDemoModeOn" class="demo-mode-indicator">
+      <p>Demo mode active.</p>
     </div>
-  </transition>
+  </teleport>
 </template>
 
 <script setup lang="ts">
@@ -17,27 +15,18 @@ const isDemoModeOn = demoMode.enabled
 </script>
 
 <style scoped>
-/* sidebars should be able to overlap it */
 .demo-mode-indicator {
   position: fixed;
-  inset: 0;
-  pointer-events: none;
-  z-index: 8;
-}
-
-.center-popup {
   position: absolute;
   top: 10px;
   left: 50%;
   transform: translateX(-50%);
   background-color: rgba(0, 0, 50, 0.85);
-  /* border: 2px solid #00bfff; */
-  /* box-shadow: 0px 0.75px 4.95px #00c4fa; */
   border-radius: 10px;
-  padding: 16px 20px;
+  padding: 6px 20px;
   color: white;
   width: 600px;
-  height: 60px;
+  height: 40px;
   text-align: center;
 }
 </style>
